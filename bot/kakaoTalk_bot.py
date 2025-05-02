ded85a4c3e6f8cb73e71157f9eee08a39ac7908b
lihuiruo101 <yjy140206@gmail.com>
2025-05-02 22:29:13 +0900

    Create kakaoTalk_bot.py

---
+++ bot/kakaoTalk_bot.py
@@ -0,0 +1,20 @@
+kakaoTalk=("https://kakao.com")
+kakaoTalk_username = {my_puture}
+my puture = bot.my_puture.py
+kakoTalk_bot
+'!!help' = help_to_kakao_bot
+kakao bot = """build up"""
+kakao bot = """pkg install python"""
+javascript = """build 3.0"""
+java_modern = """build nkdir"""
+nadir = """nkdir"""
+'!!lyrics' = lyrics_file_(mp3)_bot
+'!!AI' = OPENAI_api cloud_client
+API = (https://api_key.txt)
+if nadir < 30 = setting tool
+elif nadir > 30 = front tool
+else nadir [30] = mimic tool
+'!!response_api' = api_to_bot_
+print ("please this api to messangerbot.")
+'!!save' = save_key_road
+print ("WARNING to get")
