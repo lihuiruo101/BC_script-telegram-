425f8e0beb63c3f7eba48b2e7273b01d51c20d25
lihuiruo101 <yjy140206@gmail.com>
2025-05-02 19:57:18 +0900

    Create server.py

---
+++ bot/server.py
@@ -0,0 +1,12 @@
+python package 3.14
+javascript package 3.12
+txt package None
+shell package True
+from bot from bot.serverdesk
+server kakaoTalk = MC & 4200
+@Talkaton
+module_server 3.1.0
+version 3.1.2
+
+package True
+            =hoke messange
