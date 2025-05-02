b447bb2e96dcf9d25a3626ab81617e52357dc721
lihuiruo101 <yjy140206@gmail.com>
2025-05-02 19:50:55 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,47 @@
+from bot.__init__.py
+import random, requests
+import os, sys
+
+bot = """messanger_bot()"""
+telegram = """https://telegram.com"""
+kakaoTalk = """https://kakaotalk.com"""
+discord = """discord.gg"""
+
+(UTF-8)
+      KakaoTalk_true()
+True kakaoTalk =
+               cense bot
+
+""""bot by messenger bot""""
+(UTF-8) = True
+bot < 30 (VS code)
+
+python3.14 print ("pkg install python")
+
+flask_server
+print ("please install flask")
+messange bot_clear
+
+Java script3.4
+print ("pkg install javascript")
+
+add ()
+
+'in rest' = kakaoTalk_room_in
+(API kakaoTalk v2)
+'out rest' = kakaoTalk_room_out
+'server out rest' = kakaoTalk_room_fill_out
+'profiles' = kakaoTalk_profiles_change
+'voice' = kakaoTalk_voice_check
+
+'chat' = telegram_chat_check
+'check room' = telegram_room
+'toggle' = telegram_toggle
+
+'init' = discord_room_in
+'outit' = discord_room_out
+'serverit' = discord_server_check
+'toggleit' = discord_imoge_check
+
+Goin = ("LG U+") or ("KT")
+(/rest/)
